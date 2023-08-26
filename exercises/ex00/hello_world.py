4f642b85100eb8b21d50b84686452d087b3eda43
Joshua Locash <locashjosh@gmail.com>
2023-08-26 01:32:23 -0400

    corrected file path, fixed author var and added docstring
    Signed-off-by: Joshua Locash <locashjosh@gmail.com>

--- exercises/ex00/hello_world.py
+++
@@ -1,5 +0,0 @@
-__author__ = "Joshua Locash"
-
-# COMP-110
-# EX00: Hello World
-print("Hello World!")
---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,5 @@
+"""COMP110 - EX00 - Hello World"""
+
+__author__ = "730718389"
+
+print("hello world")
